baaed3d11a660a25097f70b47e16c56e5c5b7dc1
Sebastian Jäger <message@sebastian-jaeger.me>
2020-12-14 11:15:30 +0100

    minor changes to getting things more straight forward

--- src/data_imputation_paper/imputation.py
+++ src/data_imputation_paper/imputation.py
@@ -16,50 +16,63 @@ class BaseImputer(ABC):
     def __init__(self):
         self._fitted = False
 
+    def _is_regression_imputation(self, data: pd.DataFrame, target_column: str) -> bool:
+        return pd.api.types.is_numeric_dtype(data[target_column])
+
+    def _is_classification_imputation(self, data: pd.DataFrame, target_column: str) -> bool:
+        return pd.api.types.is_categorical_dtype(data[target_column])
+
     @abstractmethod
     def fit(self, data: pd.DataFrame, target_column: str, columns: list = [], refit: bool = False, **kwargs) -> None:
+
+        self._target_column = target_column
+        self._columns = columns
 
         # some basic error checking
         if self._fitted and not refit:
             raise ImputerError("Imputer is already fitted. Force refitting with 'refit'.")
 
-        if target_column not in data.columns:
-            raise ImputerError(f"target column '{target_column}' must be one of: {', '.join(data.columns)}")
+        if self._target_column not in data.columns:
+            raise ImputerError(f"target column '{target_column}' not found, must be one of: {', '.join(data.columns)}")
 
     @abstractmethod
-    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.Series]:
+    def transform(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
         pass
 
 
 class SKLearnModeImputer(BaseImputer):
 
+    def __init__(self, imputer_args: dict = {}):
+
+        from sklearn.impute import SimpleImputer
+
+        super().__init__()
+
+        self._imputer = SimpleImputer(**imputer_args)
+
     def fit(self, data: pd.DataFrame, target_column: str, columns: list = [], refit: bool = False, **kwargs) -> None:
 
         super().fit(data=data, target_column=target_column, columns=columns, refit=refit)
 
-        from sklearn.impute import SimpleImputer
+        # set proper strategy for column type
+        if self._is_regression_imputation(data, target_column):
+            self._imputer.set_params(strategy="mean")
 
-        # mean imputation error handling
-        if pd.api.types.is_numeric_dtype(data[target_column]):
-            self._imputer = SimpleImputer(strategy="mean", **kwargs)
+        elif self._is_classification_imputation(data, target_column):
+            self._imputer.set_params(strategy="most_frequent")
 
-        # mode imputation error handling
-        elif pd.api.types.is_categorical_dtype(data[target_column]):
-            self._imputer = SimpleImputer(strategy="most_frequent", **kwargs)
-
-        self._target_column = target_column
-        self._columns = columns
-        self._imputer.fit(data[target_column].to_numpy().reshape(-1, 1))  # sklearn expects 2D array
+        self._imputer.fit(data[target_column].to_numpy().reshape(-1, 1), **kwargs)  # sklearns' SimpleImputer expects 2D array
 
         self._fitted = True
 
-    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.Series]:
+    def transform(self, data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.Series]:
 
         completed = data.copy()
-        imputed_mask = data[self._target_column].isnull()  # TODO: does this work in all cases
+        imputed_mask = completed[self._target_column].isnull()  # TODO: does this work in all cases
 
-        # sklearn expects 2D array
+        # sklearns' SimpleImputer expects 2D array
         imputed = self._imputer.transform(completed[self._target_column].to_numpy().reshape(-1, 1))
         completed[self._target_column] = imputed
 
         return completed, imputed_mask
+
