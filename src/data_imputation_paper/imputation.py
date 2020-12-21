0a51138b8282db81ad0fc66f555fac5fc7ad2ea5
Sebastian Jäger <message@sebastian-jaeger.me>
2020-12-21 12:26:45 +0100

    move encoding/decoding method to base class

--- src/data_imputation_paper/imputation.py
+++ src/data_imputation_paper/imputation.py
@@ -25,6 +25,7 @@ class BaseImputer(ABC):
         self._fitted = False
         self._imputer = None
         self._data_encoder = None
+        self._data_encoding_type = None
 
     def _guess_dtypes(self, data: pd.DataFrame) -> Tuple[List[str], List[str]]:
         categorical_columns = [c for c in data.columns if pd.api.types.is_categorical_dtype(data[c])]
@@ -50,121 +51,6 @@ class BaseImputer(ABC):
             )
 
         return return_value
-
-    @abstractmethod
-    def _encode_data_for_imputation(self, data: pd.DataFrame, refit: bool = False) -> Tuple[pd.DataFrame, list]:
-        pass
-
-    @abstractmethod
-    def _decode_data_after_imputation(self, imputed: pd.array, index: pd.Index, dtypes: pd.Series) -> pd.DataFrame:
-        pass
-
-    @abstractmethod
-    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
-
-        self._target_column = target_column
-
-        # some basic error checking
-        if self._fitted and not refit:
-            raise ImputerError("Imputer is already fitted. Force refitting with 'refit'.")
-
-        if self._target_column not in data.columns:
-            raise ImputerError(f"target column '{target_column}' not found, must be one of: {', '.join(data.columns)}")
-
-    @abstractmethod
-    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, list]:
-        pass
-
-
-class SKLearnModeImputer(BaseImputer):
-
-    def __init__(self, imputer_args: dict = {}):
-
-        super().__init__()
-
-        self._imputer_args = imputer_args
-        self._imputer = SimpleImputer(**imputer_args)
-
-    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
-
-        super().fit(data=data, target_column=target_column, refit=refit)
-
-        # set proper strategy for column type
-        if self._is_regression_imputation(data, target_column):
-            self._imputer.set_params(strategy="mean")
-
-        elif self._is_classification_imputation(data, target_column):
-            self._imputer.set_params(strategy="most_frequent")
-
-        self._imputer.fit(data[target_column].to_numpy().reshape(-1, 1), **kwargs)  # sklearns' SimpleImputer expects 2D array
-
-        self._fitted = True
-
-    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.Series]:
-
-        completed = data.copy()
-        imputed_mask = completed[self._target_column].isnull()  # TODO: does this work in all cases
-
-        # sklearns' SimpleImputer expects 2D array
-        imputed = self._imputer.transform(completed[self._target_column].to_numpy().reshape(-1, 1), **kwargs)
-        completed[self._target_column] = imputed
-
-        return completed, imputed_mask
-
-    def _encode_data_for_imputation(self, data: pd.DataFrame, refit: bool = False) -> Tuple[pd.DataFrame, list]:
-        pass
-
-    def _decode_data_after_imputation(self, imputed: pd.array, index: pd.Index, dtypes: pd.Series) -> pd.DataFrame:
-        pass
-
-
-class SKLearnIterativeImputer(BaseImputer):
-
-    _IMPLEMENTED_STRATEGIES = ["MissForest".lower(), "MICE".lower()]
-
-    def __init__(self, strategy: str, data_encoding_type: str = "one-hot", imputer_args: dict = {}, estimator_args: dict = {}):
-
-        super().__init__()
-
-        self._strategy = strategy.lower()
-        self._imputer_args = imputer_args
-        self._estimator_args = estimator_args
-
-        if data_encoding_type in ["one-hot", "ordinal"]:
-            self._data_encoding_type = data_encoding_type
-        else:
-            raise ImputerError(f"don't know how to decode data for type '{self._data_encoding_type}'")
-
-        if "forest" in self._strategy:
-            estimator = RandomForestRegressor(**estimator_args)
-
-        elif "mice" in self._strategy:
-            estimator = BayesianRidge(**estimator_args)
-
-        else:
-            raise ImputerError(f"given strategy '{strategy}' is not implemented. Need to be one of: {', '.join(self._IMPLEMENTED_STRATEGIES)}")
-
-        self._imputer = IterativeImputer(estimator=estimator, **imputer_args)
-
-    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
-
-        super().fit(data=data, target_column=target_column, refit=refit)
-
-        encoded_data, _ = self._encode_data_for_imputation(data, refit=True)
-
-        self._imputer.fit(encoded_data, **kwargs)
-        self._fitted = True
-
-    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, list]:
-
-        encoded_data, imputed_mask = self._encode_data_for_imputation(data)
-
-        # transform returns np.array => need to create a dataframe again
-        imputed_array = self._imputer.transform(encoded_data, **kwargs)
-        completed_df = self._decode_data_after_imputation(imputed_array, data.index, data.dtypes)
-
-        # fix variable order and return imputed data and mask
-        return completed_df[data.columns], imputed_mask
 
     def _encode_data_for_imputation(self, data: pd.DataFrame, refit: bool = False) -> Tuple[pd.DataFrame, list]:
 
@@ -221,3 +107,104 @@ class SKLearnIterativeImputer(BaseImputer):
             imputed_df[column] = imputed_df[column].astype(dtypes[column].name)
 
         return imputed_df
+
+    @abstractmethod
+    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
+
+        self._target_column = target_column
+
+        # some basic error checking
+        if self._fitted and not refit:
+            raise ImputerError("Imputer is already fitted. Force refitting with 'refit'.")
+
+        if self._target_column not in data.columns:
+            raise ImputerError(f"target column '{target_column}' not found, must be one of: {', '.join(data.columns)}")
+
+    @abstractmethod
+    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, list]:
+        pass
+
+
+class SKLearnModeImputer(BaseImputer):
+
+    def __init__(self, imputer_args: dict = {}):
+
+        super().__init__()
+
+        self._imputer_args = imputer_args
+        self._imputer = SimpleImputer(**imputer_args)
+
+    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
+
+        super().fit(data=data, target_column=target_column, refit=refit)
+
+        # set proper strategy for column type
+        if self._is_regression_imputation(data, target_column):
+            self._imputer.set_params(strategy="mean")
+
+        elif self._is_classification_imputation(data, target_column):
+            self._imputer.set_params(strategy="most_frequent")
+
+        self._imputer.fit(data[target_column].to_numpy().reshape(-1, 1), **kwargs)  # sklearns' SimpleImputer expects 2D array
+
+        self._fitted = True
+
+    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, pd.Series]:
+
+        completed = data.copy()
+        imputed_mask = completed[self._target_column].isnull()  # TODO: does this work in all cases
+
+        # sklearns' SimpleImputer expects 2D array
+        imputed = self._imputer.transform(completed[self._target_column].to_numpy().reshape(-1, 1), **kwargs)
+        completed[self._target_column] = imputed
+
+        return completed, imputed_mask
+
+
+class SKLearnIterativeImputer(BaseImputer):
+
+    _IMPLEMENTED_STRATEGIES = ["MissForest".lower(), "MICE".lower()]
+
+    def __init__(self, strategy: str, data_encoding_type: str = "one-hot", imputer_args: dict = {}, estimator_args: dict = {}):
+
+        super().__init__()
+
+        self._strategy = strategy.lower()
+        self._imputer_args = imputer_args
+        self._estimator_args = estimator_args
+
+        if data_encoding_type in ["one-hot", "ordinal"]:
+            self._data_encoding_type = data_encoding_type
+        else:
+            raise ImputerError(f"don't know how to decode data for type '{self._data_encoding_type}'")
+
+        if "forest" in self._strategy:
+            estimator = RandomForestRegressor(**estimator_args)
+
+        elif "mice" in self._strategy:
+            estimator = BayesianRidge(**estimator_args)
+
+        else:
+            raise ImputerError(f"given strategy '{strategy}' is not implemented. Need to be one of: {', '.join(self._IMPLEMENTED_STRATEGIES)}")
+
+        self._imputer = IterativeImputer(estimator=estimator, **imputer_args)
+
+    def fit(self, data: pd.DataFrame, target_column: str, refit: bool = False, **kwargs) -> None:
+
+        super().fit(data=data, target_column=target_column, refit=refit)
+
+        encoded_data, _ = self._encode_data_for_imputation(data, refit=True)
+
+        self._imputer.fit(encoded_data, **kwargs)
+        self._fitted = True
+
+    def transform(self, data: pd.DataFrame, **kwargs) -> Tuple[pd.DataFrame, list]:
+
+        encoded_data, imputed_mask = self._encode_data_for_imputation(data)
+
+        # transform returns np.array => need to create a dataframe again
+        imputed_array = self._imputer.transform(encoded_data, **kwargs)
+        completed_df = self._decode_data_after_imputation(imputed_array, data.index, data.dtypes)
+
+        # fix variable order and return imputed data and mask
+        return completed_df[data.columns], imputed_mask
