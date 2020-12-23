56b51203127981f48b5edb1251c09209aaa31e2a
Sebastian Jäger <message@sebastian-jaeger.me>
2020-12-23 10:28:32 +0100

    Add logging setup

--- src/data_imputation_paper/__init__.py
+++ src/data_imputation_paper/__init__.py
@@ -9,3 +9,24 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+
+import sys
+import logging
+
+# setup logging
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("[%(asctime)s - %(filename)-17s:%(lineno)-4s - %(levelname)-7s - %(funcName)-35s]: %(message)s")
+
+info_handler = logging.StreamHandler(sys.stdout)
+info_handler.setLevel(logging.INFO)
+info_handler.setFormatter(formatter)
+
+error_handler = logging.StreamHandler(sys.stderr)
+error_handler.setLevel(logging.WARNING)
+error_handler.setFormatter(formatter)
+
+logger.addHandler(error_handler)
+logger.addHandler(info_handler)
