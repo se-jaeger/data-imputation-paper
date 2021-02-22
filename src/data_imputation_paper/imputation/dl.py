2122f3b13e19e02986db345a3a750fe71ca26a02
Sebastian Jäger <message@sebastian-jaeger.me>
2021-02-22 16:31:12 +0100

    minor changes and improvements

--- src/data_imputation_paper/imputation/dl.py
+++ src/data_imputation_paper/imputation/dl.py
@@ -1,14 +1,15 @@
-from typing import Optional, List, Tuple
-
-from autokeras import StructuredDataClassifier, StructuredDataRegressor
-
 import logging
+from typing import Dict, List, Optional, Tuple
+
 import pandas as pd
+import tensorflow as tf
+from autokeras import StructuredDataClassifier, StructuredDataRegressor
+from tensorflow.keras import Model
 
 from ._base import BaseImputer
 
-
 logger = logging.getLogger()
+tf.get_logger().setLevel('ERROR')
 
 
 class AutoKerasImputer(BaseImputer):
@@ -16,19 +17,19 @@ class AutoKerasImputer(BaseImputer):
     def __init__(
         self,
         max_trials: Optional[int] = 10,
-        tuner: Optional[str] = 'greedy',
-        validation_split: Optional[float] = 0.1,
+        tuner: Optional[str] = None,
+        validation_split: Optional[float] = 0.2,
         epochs: Optional[int] = 10,
         seed: Optional[int] = None
     ):
         """
-        Imputer uses `AutoKeras` 
+        Deep Learning-learning based imputation mehtod. It uses AutoKeras to find good architecture/hyperparameters.
 
         Args:
-            max_trials (Optional[int] = 10): maximum number of trials for model selection
-            tuner (Optional[str] = 'greedy'): autokeras hyperparameter tuning strategy
-            validiation_split (Optional[float] = 0.1): validation split for autokeras fit
-            epochs (Optional[int] = 10): number of epochs for autokeras fit
+            max_trials (Optional[int], optional): maximum number of trials for model selection. Defaults to 10.
+            tuner (Optional[str], optional): AutoKeras hyperparameter tuning strategy. Defaults to None.
+            validation_split (Optional[float], optional): validation split for AutoKeras fit. Defaults to 0.2.
+            epochs (Optional[int], optional): number of epochs for AutoKeras fit. Defaults to 10.
             seed (Optional[int], optional): Seed to make behavior deterministic. Defaults to None.
         """
 
@@ -40,12 +41,16 @@ class AutoKerasImputer(BaseImputer):
         self.epochs = epochs
         self.validation_split = validation_split
         self.tuner = tuner
-        self._predictors = {}
+        self._predictors: Dict[str, Model] = {}
 
     def get_best_hyperparameters(self):
 
-        # TODO..
-        pass
+        super().get_best_hyperparameters()
+
+        return {
+            column: self._predictors[column].tuner.get_best_hyperparameters()[0].values
+            for column in self._predictors.keys()
+        }
 
     def fit(self, data: pd.DataFrame, target_columns: List[str]) -> BaseImputer:
 
@@ -54,27 +59,27 @@ class AutoKerasImputer(BaseImputer):
         # cast categorical columns to strings fixes problems where categories are integer values and treated as regression task
         data = self._categorical_columns_to_string(data.copy())  # We don't want to change the input dataframe -> copy it
 
-        for target in self._target_columns:
+        for target_column in self._target_columns:
 
-            feature_cols = [c for c in self._categorical_columns + self._numerical_columns if c != target]
+            missing_mask = data[target_column].isna()
+            feature_cols = [c for c in self._categorical_columns + self._numerical_columns if c != target_column]
 
-            if target in self._numerical_columns:
-                StructuredDataModel = StructuredDataRegressor
+            if target_column in self._numerical_columns:
+                StructuredDataModelSearch = StructuredDataRegressor
 
-            elif target in self._categorical_columns:
-                StructuredDataModel = StructuredDataClassifier
+            elif target_column in self._categorical_columns:
+                StructuredDataModelSearch = StructuredDataClassifier
 
-            self._predictors[target] = StructuredDataModel(
-                    column_names=feature_cols,
-                    overwrite=True,
-                    max_trials=self.max_trials,
-                    tuner=self.tuner
-                )
+            self._predictors[target_column] = StructuredDataModelSearch(
+                column_names=feature_cols,
+                overwrite=True,
+                max_trials=self.max_trials,
+                tuner=self.tuner
+            )
 
-            missing_mask = data[target].isna()
-            self._predictors[target].fit(
+            self._predictors[target_column].fit(
                 x=data.loc[~missing_mask, feature_cols],
-                y=data.loc[~missing_mask, target],
+                y=data.loc[~missing_mask, target_column],
                 epochs=self.epochs
             )
 
@@ -94,14 +99,14 @@ class AutoKerasImputer(BaseImputer):
         # ... dtypes of data need to be same as for fitting
         data = self._categorical_columns_to_string(data.copy())  # We don't want to change the input dataframe -> copy it
 
-        for target in self._target_columns:
-            feature_cols = [c for c in self._categorical_columns + self._numerical_columns if c != target]
-            missing_mask = data[target].isna()
+        for target_column in self._target_columns:
+            feature_cols = [c for c in self._categorical_columns + self._numerical_columns if c != target_column]
+            missing_mask = data[target_column].isna()
             amount_missing_in_columns = missing_mask.sum()
 
             if amount_missing_in_columns > 0:
-                data.loc[missing_mask, target] = self._predictors[target].predict(data.loc[missing_mask, feature_cols])
-                logger.debug(f'Imputed {amount_missing_in_columns} values in column {target}')
+                data.loc[missing_mask, target_column] = self._predictors[target_column].predict(data.loc[missing_mask, feature_cols])
+                logger.debug(f'Imputed {amount_missing_in_columns} values in column {target_column}')
 
         self._restore_dtype(data, dtypes)
 
