75a3c002818ecc28e510c0ea856706883d3b395e
Sebastian Jäger <message@sebastian-jaeger.me>
2021-03-05 17:57:06 +0100

    Introduce Experiment where we also iterate over different settings

--- src/data_imputation_paper/experiment.py
+++ src/data_imputation_paper/experiment.py
@@ -6,17 +6,27 @@ from pathlib import Path
 from typing import Callable, Dict, List, Optional, Tuple
 
 import joblib
-from jenga.corruptions.generic import MissingValues
 from jenga.tasks.openml import OpenMLTask
 
-from .evaluation import EvaluationResult, Evaluator
+from .evaluation import (
+    EvaluationResult,
+    MultipleColumnsAllMissingEvaluator,
+    MultipleColumnsEvaluator,
+    SingleColumnAllMissingEvaluator,
+    SingleColumnEvaluator
+)
 from .imputation._base import BaseImputer
 from .imputation.utils import set_seed
 
 logger = logging.getLogger()
 
-# TODO: next steps:
-# - Plotting results..
+
+strategy_to_EvaluatorClass = {
+    "single_single": SingleColumnEvaluator,
+    "multiple_multiple": MultipleColumnsEvaluator,
+    "single_all": SingleColumnAllMissingEvaluator,
+    "multiple_all": MultipleColumnsAllMissingEvaluator
+}
 
 
 class Experiment(object):
@@ -26,6 +36,7 @@ class Experiment(object):
         task_id_class_tuples: List[Tuple[int, Callable[..., OpenMLTask]]],
         missing_fractions: List[float],
         missing_types: List[str],
+        strategies: List[str],
         imputer_class: BaseImputer,
         imputer_arguments: dict,
         num_repetitions: int,
@@ -37,12 +48,18 @@ class Experiment(object):
         self._task_id_class_tuples = task_id_class_tuples
         self._missing_fractions = missing_fractions
         self._missing_types = missing_types
+        self._strategies = strategies
         self._imputer_class = imputer_class
         self._imputer_arguments = imputer_arguments
         self._num_repetitions = num_repetitions
         self._timestamp = timestamp
         self._seed = seed
         self._result: Dict[int, Dict[str, Dict[float, Dict[str, EvaluationResult]]]] = dict()
+
+        valid_strategies = strategy_to_EvaluatorClass.keys()
+        for strategy in self._strategies:
+            if strategy not in valid_strategies:
+                raise Exception(f"'{strategy}' is not a valid strategy. Need to be in {', '.join(valid_strategies)}")
 
         self._base_path = Path(base_path)
 
@@ -67,27 +84,34 @@ class Experiment(object):
                 self._result[task_id][missing_type] = {}
 
                 for missing_fraction in self._missing_fractions:
-                    # TODO: We probably want to change this
-                    missing_values = [
-                        MissingValues(column=column, fraction=missing_fraction, missingness=missing_type)
-                        for column in task.train_data.columns
-                    ]
+                    self._result[task_id][missing_type][missing_fraction] = {}
 
-                    experiment_path = self._base_path / f"{task_id}" / missing_type / f"{missing_fraction}"
+                    for strategy in self._strategies:
 
-                    try:
-                        evaluator = Evaluator(task, missing_values, self._imputer_class, self._imputer_arguments, experiment_path, seed=None)
-                        evaluator.evaluate(self._num_repetitions)
-                        result = evaluator._result
+                        experiment_path = self._base_path / f"{task_id}" / missing_type / f"{missing_fraction}" / f"{strategy}"
 
-                    except Exception:
-                        error = traceback.format_exc()
-                        experiment_path.mkdir(parents=True, exist_ok=True)
-                        Path(experiment_path / "error.txt").write_text(str(error))
-                        logger.exception(f"Tried to run - missing type: {missing_type} - missing fraction: {missing_fraction}")
-                        result = error
+                        try:
+                            evaluator = strategy_to_EvaluatorClass[strategy](
+                                task=task,
+                                missing_fraction=missing_fraction,
+                                missing_type=missing_type,
+                                target_column=task.train_data.columns.tolist()[0] if "single" in strategy else task.train_data.columns.tolist()[:2],  # TODO We need to find a way to sample here!
+                                imputer_class=self._imputer_class,
+                                imputer_args=self._imputer_arguments,
+                                path=experiment_path,
+                                seed=None
+                            )
+                            evaluator.evaluate(self._num_repetitions)
+                            result = evaluator._result
 
-                    self._result[task_id][missing_type][missing_fraction] = result
+                        except Exception:
+                            error = traceback.format_exc()
+                            experiment_path.mkdir(parents=True, exist_ok=True)
+                            Path(experiment_path / "error.txt").write_text(str(error))
+                            logger.exception(f"Tried to run - missing type: {missing_type} - missing fraction: {missing_fraction}")
+                            result = error
+
+                        self._result[task_id][missing_type][missing_fraction][strategy] = result
 
         joblib.dump(self._result, Path(self._base_path / f"{task_id}" / "result.joblib"))
         Path(self._base_path / f"{task_id}" / "evaluation_parameters.json").write_text(
@@ -95,7 +119,7 @@ class Experiment(object):
                 {
                     "missing_types": self._missing_types,
                     "missing_fractions": self._missing_fractions,
-                    "target_columns": list(task.train_data.columns)  # TODO: If we change this above also change it here!
+                    "strategy": self._strategies
                 }
             )
         )
